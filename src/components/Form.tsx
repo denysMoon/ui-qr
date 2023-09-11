a2798e9414a199e906f5cbff0cb2e4790e4303c7
Denis Ulvanskyi <denis.ulvanskyi@cengage.com>
2023-09-11 15:32:24 +0300

    used yup for validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,25 @@
 import { useForm, SubmitHandler } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Button, Form as FormBootstrap } from "react-bootstrap";
 
 type Inputs = {
   qrString: string;
-  required: string;
 };
 
 interface IForm {
   getBase64: (searchString: string) => Promise<void>;
 }
+
+const schema = yup
+  .object({
+    qrString: yup
+      .string()
+      .required("Type something")
+      .min(2, "Must be at least 2 characters"),
+    required: yup.string(),
+  })
+  .required();
 
 export const Form: React.FC<IForm> = ({ getBase64 }) => {
   const {
@@ -16,7 +27,7 @@ export const Form: React.FC<IForm> = ({ getBase64 }) => {
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm<Inputs>();
+  } = useForm<Inputs>({ resolver: yupResolver(schema) });
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     getBase64(data.qrString);
@@ -24,18 +35,19 @@ export const Form: React.FC<IForm> = ({ getBase64 }) => {
   };
 
   return (
-    <FormBootstrap onSubmit={handleSubmit(onSubmit)} className="formWrapper">
-      <FormBootstrap.Control
-        type="text"
-        placeholder="Type here"
-        aria-label="Form input"
-        {...register("qrString")}
-      />
-      {errors.required && <span>This field is required</span>}
-
-      <Button type="submit" size="lg" className="formButton">
-        Get
-      </Button>
+    <FormBootstrap onSubmit={handleSubmit(onSubmit)}>
+      <div className="formWrapper">
+        <FormBootstrap.Control
+          type="text"
+          placeholder="Type here"
+          aria-label="Form input"
+          {...register("qrString")}
+        />
+        <Button type="submit" size="lg" className="formButton">
+          Get
+        </Button>
+      </div>
+      {errors.qrString && <span>{errors.qrString?.message}</span>}
     </FormBootstrap>
   );
 };
